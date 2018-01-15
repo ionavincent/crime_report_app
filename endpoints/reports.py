10d856ca545a274d0eb5099859da2a524a6fba00
Iona Vincent <ionagvincent@gmail.com>
2018-01-15 19:52:10 +0000

    update parameters and frontend

--- endpoints/reports.py
+++ endpoints/reports.py
@@ -1,8 +1,11 @@
 from flask import request
 from flask_restplus import Resource
+from sqlalchemy import and_
+
 
 from restplus import api
 from endpoints.serializers import report
+from endpoints.parsers import reports_request_parser
 from database.models import CrimeReport
 
 ns = api.namespace('reports',
@@ -13,13 +16,14 @@ ns = api.namespace('reports',
 class ReportCollection(Resource):
 
     @ns.marshal_list_with(report)
+    @api.expect(reports_request_parser, validate=True)
     def get(self):
         """
         Returns a list of all crime types, filtered by year, if provided
         """
-        # TODO: Replace with parameters (api.parameters)!
-        limit = request.args.get("limit")
-        limit = int(limit)
+        args = reports_request_parser.parse_args()
+
+        limit = args.get("limit")
 
         valid_orderings = {
             "crime_type": CrimeReport.crime_type,
@@ -28,9 +32,23 @@ class ReportCollection(Resource):
         }
 
         valid_filters = {
-            "year": lambda x: CrimeReport.year.like(int(x)),
-            "month": lambda x: CrimeReport.month.like(int(x)),
-            "crime_type": lambda x: CrimeReport.crime_type.like(x)
+            "crime_id": lambda value: CrimeReport.crime_id.like(value),
+            "year": lambda value: CrimeReport.year.like(value),
+            "month": lambda value: CrimeReport.month.like(value),
+            "reported_by": lambda value: CrimeReport.reported_by.like(value),
+            "falls_within": lambda value: CrimeReport.falls_within.like(value),
+            "longitude": lambda value: CrimeReport.longitude.like(value),
+            "latitude": lambda value: CrimeReport.latitude.like(value),
+            "location": lambda value: CrimeReport.location.like(
+                "%{}%".format(value)
+            ),
+            "lsoa_code": lambda value: CrimeReport.lsoa_code.like(value),
+            "lsoa_name": lambda value: CrimeReport.lsoa_name.like(value),
+            "crime_type": lambda value: CrimeReport.crime_type.like(value),
+            "last_outcome": lambda value: CrimeReport.last_outcome.like(
+                "%{}%".format(value)
+            ),
+            "context": lambda value: CrimeReport.context.like(value)
         }
 
         ordering = valid_orderings.get(
@@ -39,11 +57,13 @@ class ReportCollection(Resource):
         )
 
         filters = []
-        for parameter_name, value in request.args.items():
-            if parameter_name in valid_filters and len(value) > 0:
+        for parameter_name, value in args.items():
+            if parameter_name in valid_filters and value:
                 filters.append(valid_filters[parameter_name](value))
 
-        reports = (CrimeReport.query.filter(*filters)
+        print(filters)
+
+        reports = (CrimeReport.query.filter(and_(*filters))
                    .order_by(ordering)
                    .limit(limit)
                    .all())
